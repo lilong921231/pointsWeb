dd44b31a1bcbfa9d243a58bab8158b715e2bbde8
lilong <568639606@qq.com>
2019-01-19 22:12:46 +0800

    路由守卫

---
+++ src/app/components/user/user-common.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {UserMainComponent} from './currency/user-main/user-main.component';
+import {UserRegComponent} from './currency/user-reg/user-reg.component';
+import {UserUserInfoComponent} from './currency/user-user-info/user-user-info.component';
+import {UserMessageComponent} from './currency/user-message/user-message.component';
+import {UserTbKlineComponent} from './currency/user-tb-kline/user-tb-kline.component';
+import {UserRepassComponent} from './currency/user-repass/user-repass.component';
+import {UserPointComponent} from './currency/user-point/user-point.component';
+import {UserMoneyComponent} from './currency/user-money/user-money.component';
+import {UserLoginComponent} from './user-login/user-login.component';
+import {UserMenuComponent} from './common/user-menu/user-menu.component';
+import {UserHeaderComponent} from './common/user-header/user-header.component';
+import {UserMessageInfoComponent} from './currency/user-message-info/user-message-info.component';
+import {MomentModule} from 'ngx-moment';
+import {NgxEchartsModule} from 'ngx-echarts';
+import {CKEditorModule} from 'ng2-ckeditor';
+import {PaginationModule} from 'ngx-bootstrap';
+import {AppRoutingModule} from '../../app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from '../admin/login/login.component';
+
+/**
+ * @desc user端公共组件及声明模块
+ * @author lilong
+ * @date 2019-1-18
+ */
+
+@NgModule({
+  declarations: [
+    UserMainComponent,
+    UserRegComponent,
+    UserUserInfoComponent,
+    UserMessageComponent,
+    UserTbKlineComponent,
+    UserRepassComponent,
+    UserPointComponent,
+    UserMoneyComponent,
+    UserLoginComponent,
+    UserMenuComponent,
+    UserHeaderComponent,
+    UserMessageInfoComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    MomentModule,
+    NgxEchartsModule,
+    CKEditorModule,
+    HttpClientModule,
+    PaginationModule.forRoot(),
+  ],
+  exports: [
+    HttpClientModule,
+    CKEditorModule,
+    MomentModule,
+    NgxEchartsModule,
+    UserMainComponent,
+    UserRegComponent,
+    UserUserInfoComponent,
+    UserMessageComponent,
+    UserTbKlineComponent,
+    UserRepassComponent,
+    UserPointComponent,
+    UserMoneyComponent,
+    UserLoginComponent,
+    UserMenuComponent,
+    UserHeaderComponent,
+    UserMessageInfoComponent,
+  ]
+})
+export class UserCommonModule { }
---
+++ src/app/components/user/user-login.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {HttpService} from '../../common/service/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserLoginGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private http: HttpService
+  ) {}
+
+  isLogin = false;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+
+    const path = route.routeConfig.path;
+    // nextRoute: 设置需要路由守卫的路由集合
+    const nextRoute = ['main', 'repass', 'userInfo', 'reg', 'money_user_list', 'point', 'message', 'messageInfo/:id', 'tb-kline'];
+
+    if (this.http.getCookie('user') === 'user') {
+      this.isLogin = true;
+      return this.isLogin;
+    }
+    // 当前路由是nextRoute指定页时
+    if (nextRoute.indexOf(path) >= 0) {
+      if (!this.isLogin) {
+        // 未登录，跳转到login
+        this.router.navigate(['']);
+        return false;
+      } else {
+        // 已登录，跳转到当前路由
+        return true;
+      }
+    }
+  }
+}
